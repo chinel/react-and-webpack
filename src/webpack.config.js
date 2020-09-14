6b8412674c51fb01a271d81e64ec4fdeccd2ad44
nelo <nelokwaz@gmail.com>
2020-09-14 18:04:42 +0100

    installed the html loader

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -8,6 +8,11 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
