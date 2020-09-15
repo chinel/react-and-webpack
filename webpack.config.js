98fa9869dec3d36f690920a08300e21de843909d
nelo <nelokwaz@gmail.com>
2020-09-15 07:18:05 +0100

    moved the babel and webpack file to the root directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "index.js",
-  output: {
-    filename: "bundle.js",
-  },
   module: {
     rules: [
       {
